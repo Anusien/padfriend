4e0c2f285242d9f8cafeb080c554f98cf262cc9a
Kevin Binswanger <kbinswanger@gmail.com>
2014-07-06 23:19:24 -0500

    Login/logout stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+  	if signed_in?
+		redirect_to current_user
+	else	
+		redirect_to signin_path
+	end
+  end
+
   def show 
   	@user = User.find(params[:id])
   end
